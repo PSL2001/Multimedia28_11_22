3e574d75d38d1e54db631f3d2cca7e34d92a9d86
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2022-11-28 13:14:03 +0100

    Comienzo Proyecto: Añadido Audio y Layout

--- app/src/main/java/com/example/audio28_11_22/MainActivity.kt
+++ app/src/main/java/com/example/audio28_11_22/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.audio28_11_22
 
+import android.content.Context
+import android.media.AudioManager
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.audio28_11_22.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var mp: MediaPlayer
+    lateinit var audioManager: AudioManager
+    var pause = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnPause.isEnabled = false
+        mp = MediaPlayer.create(this, R.raw.cancion1)
+        setListeners()
+        iniciarVolumen()
+    }
+
+    private fun iniciarVolumen() {
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+    }
+
+    private fun setListeners() {
+        binding.btnPlay.setOnClickListener {
+            startCancion()
+        }
+        binding.btnPause.setOnClickListener {
+            pausarCancion()
+        }
+        binding.btnInicio.setOnClickListener {
+            inicioCancion()
+        }
+    }
+
+    private fun inicioCancion() {
+        if (mp != null) {
+            mp.seekTo(0);
+        }
+    }
+
+    private fun pausarCancion() {
+        binding.btnPlay.isEnabled = true
+        binding.btnPause.isEnabled = false
+        mp.pause()
+    }
+
+    private fun startCancion() {
+        binding.btnPlay.isEnabled = false
+        binding.btnPause.isEnabled = true
+        mp.start()
     }
 }
